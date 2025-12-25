bd367d0b6ed15d0bc7fa36ae1ca59730847f792b
MOHAMED ASVATH <www.mohamedasvath@gmail.com>
2025-12-25 17:22:47 +0530

    Present and absent change

--- src/components/AttendanceGrid.jsx
+++ src/components/AttendanceGrid.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import { useState } from "react";
 import AttendanceModal from "./AttendanceModal";
 
-const STATUS = ["", "P", "A", "L"];
+const STATUS = ["", "ح", "غ", "L"];
 
 const AttendanceGrid = ({ students, onEdit }) => {
 
